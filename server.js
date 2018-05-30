660ffa078bff81dd7ce8be484d6a47af609562b4
Strangebrewer <BKAShambala@gmail.com>
2018-05-30 14:03:06 -0600

    change server file to stop resyncing tables

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("view engine", "handlebars");
 //  Give the server access to the routes imported above.
 app.use(routes);
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync({ force: false }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
